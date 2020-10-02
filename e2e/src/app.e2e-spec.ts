7ef3c4825d00862289249fb2cf44ddce1732a197
Christian Garbs <mitch@cgarbs.de>
2020-10-02 21:56:59 +0200

    fix e2e tests; switch to Firefox

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,23 +1,25 @@
 import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
 
-describe('workspace-project App', () => {
+describe('workspace-project PlaceHolder', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display title screen', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('app app is running!');
+    expect(page.getTitleText()).toEqual('PlaceHolder');
   });
 
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
-  });
+  // TODO: This fails with Firefox, longstanding bug in Protractor
+  // https://github.com/angular/protractor/issues/5346
+  // afterEach(async () => {
+  //   // Assert that there are no errors emitted from the browser
+  //   const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+  //   expect(logs).not.toContain(jasmine.objectContaining({
+  //     level: logging.Level.SEVERE,
+  //   } as logging.Entry));
+  // });
 });
